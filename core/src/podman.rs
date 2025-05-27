f4b60f7ef547543d0b92f4838920313d2f30fc24
VoidArc Studio <chudybyczek2137@gmail.com>
2025-05-27 18:56:01 +0200

    Update podman.rs

--- core/src/podman.rs
+++ core/src/podman.rs
@@ -3,18 +3,17 @@ use std::process::Command;
 
 pub fn cleanup_tmp_session(session_id: &str) {
     let session_dir = format!("/tmp/penmode-session-{}", session_id);
-    let _ = fs::remove_dir_all(&session_dir); // Ignoruje błędy, jeśli katalog nie istnieje
+    std::fs::remove_dir_all(&session_dir).unwrap_or(());
 }
 
+#[allow(dead_code)]
 pub fn limit_resources(session_id: &str, cpu: f32, memory: u64) {
-    let _ = Command::new("podman")
-        .args([
-            "update",
-            "--cpus",
-            &cpu.to_string(),
-            "--memory",
-            &format!("{}m", memory),
-            session_id,
-        ])
-        .output(); // Ignoruje błędy, jeśli komenda nie powiedzie się
+    let mut cmd = Command::new("podman");
+    cmd.args([
+        "update",
+        &format!("--cpus={}", cpu),
+        &format!("--memory={}m", memory),
+        session_id,
+    ]);
+    cmd.spawn().ok();
 }
